1f5df8a9ce2b467279e646790bb9a0609ad6d3ab
Benjamintxt <benjamincaseych@protonmail.com>
2023-12-11 15:13:05 +0100

    Implementation de cohere classify

--- technoemergentes/lib/main.dart
+++ technoemergentes/lib/main.dart
@@ -1,125 +1,134 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+class CustomerSupportPage extends StatefulWidget {
+  @override
+  _CustomerSupportPageState createState() => _CustomerSupportPageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _CustomerSupportPageState extends State<CustomerSupportPage> {
+  final TextEditingController _textInputController = TextEditingController();
+  List<Map<String, dynamic>> _results = [];
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Future<void> classifyText(String text) async {
+    const apiKey = 'nWrwm3g8q4ZIWzDzCQZTbup9RCt87oFsu7yLt7XB'; 
+    const apiUrl = 'https://api.cohere.ai/v1/classify';
+    final model = 'embed-multilingual-v3.0';
+
+    final examples = [
+      {"text": "Dégradation des espaces publics dans le quartier", "label": "signalement urbain"},
+      {"text": "Nids-de-poule dangereux sur la route principale", "label": "signalement sur la chaussée"},
+      {"text": "Installation de bancs publics nécessaires dans le parc", "label": "proposition d'idée"},
+      {"text": "Tags sur les murs du centre-ville", "label": "signalement urbain"},
+      {"text": "Feux de signalisation défectueux à l'intersection", "label": "signalement sur la chaussée"},
+      {"text": "Création d'une piste cyclable pour améliorer la sécurité", "label": "proposition d'idée"},
+      {"text": "Dépôt illégal d'ordures près de la station de bus", "label": "signalement urbain"},
+      {"text": "Trous dans le trottoir près de l'école", "label": "signalement sur la chaussée"},
+      {"text": "Ajout de panneaux de signalisation pour la sécurité des piétons", "label": "proposition d'idée"},
+      {"text": "Éclairage public insuffisant dans le quartier résidentiel", "label": "signalement urbain"},
+      {"text": "Route glissante en raison de l'accumulation d'eau", "label": "signalement sur la chaussée"},
+      {"text": "Création d'un jardin communautaire dans le parc", "label": "proposition d'idée"},
+      {"text": "Défauts dans l'aménagement paysager du centre-ville", "label": "signalement urbain"},
+      {"text": "Trous dans le revêtement de la chaussée près du marché", "label": "signalement sur la chaussée"},
+      {"text": "Installation de stations de recharge pour véhicules électriques", "label": "proposition d'idée"},
+      {"text": "Mauvaise gestion des déchets dans les espaces publics", "label": "signalement urbain"},
+      {"text": "Marquages routiers effacés près de l'école primaire", "label": "signalement sur la chaussée"},
+      {"text": "Création d'une aire de jeux pour enfants dans le parc", "label": "proposition d'idée"},
+      {"text": "Éclairage défectueux dans le quartier commercial", "label": "signalement urbain"},
+      {"text": "Route bloquée en raison de travaux de réparation", "label": "signalement sur la chaussée"},
+      {"text": "Suggestions pour la rénovation du marché local", "label": "proposition d'idée"},
+      {"text": "Décharge sauvage près de la rivière", "label": "signalement urbain"},
+      {"text": "Manque de passages piétons sécurisés près des écoles", "label": "signalement sur la chaussée"},
+      {"text": "Création d'un espace de loisirs pour les jeunes", "label": "proposition d'idée"},
+      {"text": "Vandalisme sur les bancs publics du parc", "label": "signalement urbain"},
+      {"text": "Signalisation inadéquate à l'entrée du quartier", "label": "signalement sur la chaussée"},
+      {"text": "Aménagement d'un parc canin dans le quartier", "label": "proposition d'idée"},
+      {"text": "Graffiti sur les arrêts de bus", "label": "signalement urbain"},
+      {"text": "Réparation nécessaire des trottoirs dans le centre-ville", "label": "signalement sur la chaussée"},
+      {"text": "Création d'un festival artistique annuel", "label": "proposition d'idée"},
+    ];
+
+
+    final requestBody = {
+      "model": model,
+      "inputs": [text],
+      "examples": examples,
+    };
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      body: jsonEncode(requestBody),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      print(data);
+      final results = data['results'];
+      setState(() {
+  _results = List<Map<String, dynamic>>.from(data['classifications']);
     });
+    } else {
+      // Handle error
+      print('Failed to classify text. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Customer Support'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _textInputController,
+              decoration: InputDecoration(labelText: 'Enter your request'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () async {
+                final inputText = _textInputController.text.trim();
+                if (inputText.isNotEmpty) {
+                  await classifyText(inputText);
+                }
+              },
+              child: Text('Submit Request'),
+            ),
+            SizedBox(height: 16.0),
+            Text('Classification Results:'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _results.length,
+                itemBuilder: (context, index) {
+                  final result = _results[index];
+                  final inputText = result['input'];
+
+                  return ListTile(
+                    title: Text(inputText),
+                    subtitle: Text('Prediction: ${result['prediction']}'),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: CustomerSupportPage(),
+  ));
 }
