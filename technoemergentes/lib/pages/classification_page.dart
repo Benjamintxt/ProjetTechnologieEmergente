c9d421e6eddc8472576bbc6e035a35b542406a8d
Benjamintxt <benjamincaseych@protonmail.com>
2023-12-14 11:55:17 +0100

    Classification page updated (fetch from firebase)

--- technoemergentes/lib/pages/classification_page.dart
+++ technoemergentes/lib/pages/classification_page.dart
@@ -1,34 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:technoemergentes/services/firestoreService.dart';
 
-class ClassificationPage extends StatelessWidget {
+
+class ClassificationPage extends StatefulWidget {
+  @override
+  _ClassificationPageState createState() => _ClassificationPageState();
+}
+
+class _ClassificationPageState extends State<ClassificationPage> {
+  final FirestoreService _firestoreService = FirestoreService();
+  late List<DocumentSnapshot> _complaints = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadComplaints();
+  }
+  
+  Future<void> _loadComplaints() async {
+    List<DocumentSnapshot> complaints = await _firestoreService.getDocuments();
+    setState(() {
+      _complaints = complaints;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Complaints Classification'),
       ),
-      body: ListView(
-        children: [
-          Card(
+      body: ListView.builder(
+        itemCount: _complaints.length,
+        itemBuilder: (context, index) {
+          final complaint = _complaints[index].data() as Map<String, dynamic>;
+          final inputText = complaint['demande'];
+          final prediction = complaint['label'];
+
+          return Card(
             child: ListTile(
-              title: Text('Issue 1'),
-              subtitle: Text('Complaint 1 from main.dart'),
+              title: Text('Demande ${index + 1}'),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Text: $inputText'),
+                  Text('Prediction: $prediction'),
+                ],
+              ),
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Issue 2'),
-              subtitle: Text('Complaint 2 from main.dart'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('Issue 3'),
-              subtitle: Text('Complaint 3 from main.dart'),
-            ),
-          ),
-          // Add more cards for other complaints from main.dart
-        ],
+          );
+        },
       ),
     );
   }
